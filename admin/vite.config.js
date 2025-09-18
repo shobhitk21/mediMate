10bd3b3c7a54d7baf42af06cfbff9aa82da890b5
Shobhit Katiyar <Shobhitkatiyar639@gmail.com>
2025-09-18 23:14:46 +0530

    frontend deployment

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,9 +1,43 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss(),],
+//   server: { port: 5174 }
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
-  server: { port: 5174 }
-})
+  plugins: [react(), tailwindcss()],
+  server: {
+    port: 5173,
+    fs: {
+      allow: ['..']  // allow accessing parent folders
+    }
+  },
+  resolve: {
+    alias: {
+      '@admin': path.resolve(__dirname, '../admin/src')
+    }
+  },
+  optimizeDeps: {
+    include: ['axios']
+  },
+  build: {
+    commonjsOptions: {
+      include: [/node_modules/, /admin/]
+    },
+    rollupOptions: {
+      // leave external empty unless needed
+      external: []
+    }
+  }
+});
+
+
