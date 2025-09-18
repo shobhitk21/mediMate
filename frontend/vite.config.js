39838fb91e464d0b2a3ffc8a260402e03f822dcd
Shobhit Katiyar <Shobhitkatiyar639@gmail.com>
2025-09-18 23:05:17 +0530

    frontend deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,43 +1,43 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react(),
-//   tailwindcss()],
-//   server: { port: 5173 }
-// })
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    port: 5173,
-    fs: {
-      allow: ['..']  // allow accessing parent folders
-    }
-  },
-  resolve: {
-    alias: {
-      '@admin': path.resolve(__dirname, '../admin/src')
-    }
-  },
-  optimizeDeps: {
-    include: ['axios']
-  },
-  build: {
-    commonjsOptions: {
-      include: [/node_modules/, /admin/]
-    },
-    rollupOptions: {
-      // leave external empty unless needed
-      external: []
-    }
-  }
-});
+  plugins: [react(),
+  tailwindcss()],
+  server: { port: 5173 }
+})
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+// import path from 'path'
+
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   server: {
+//     port: 5173,
+//     fs: {
+//       allow: ['..']  // allow accessing parent folders
+//     }
+//   },
+//   resolve: {
+//     alias: {
+//       '@admin': path.resolve(__dirname, '../admin/src')
+//     }
+//   },
+//   optimizeDeps: {
+//     include: ['axios']
+//   },
+//   build: {
+//     commonjsOptions: {
+//       include: [/node_modules/, /admin/]
+//     },
+//     rollupOptions: {
+//       // leave external empty unless needed
+//       external: []
+//     }
+//   }
+// });
 
