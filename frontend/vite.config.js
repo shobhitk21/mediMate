b23873bf776a68938631274975d170ceffbfa8c6
Shobhit Katiyar <Shobhitkatiyar639@gmail.com>
2025-09-20 11:13:09 +0530

    changes for hosting

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,5 +6,10 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(),
   tailwindcss()],
+  build: {
+    rollupOptions: {
+      external: ['axios']
+    }
+  },
   server: { port: 5173 }
 })
