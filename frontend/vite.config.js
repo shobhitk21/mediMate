7174e79781b112d032f1e0272927dce09f136b55
Shobhit Katiyar <Shobhitkatiyar639@gmail.com>
2025-09-21 11:22:30 +0530

    deploying frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,9 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(),
   tailwindcss()],
+  optimizeDeps: {
+    include: ["axios"],
+  },
   build: {
     rollupOptions: {
       external: ['axios', 'react-toastify']
@@ -13,3 +16,19 @@ export default defineConfig({
   },
   server: { port: 5173 }
 })
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   optimizeDeps: {
+//     include: ["axios"],
+//   },
+//   build: {
+//     rollupOptions: {
+//       external: [],
+//     },
+//   },
+// });
+
