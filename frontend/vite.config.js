26e959e919b31ac134458d109563024147df3e02
Shobhit Katiyar <Shobhitkatiyar639@gmail.com>
2025-09-21 20:11:42 +0530

    fixing error while refresh

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,10 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: './', // super important for static hosting like Vercel
-  optimizeDeps: {
-    include: ["axios"],
-  },
+  base: '/',  
   build: {
     outDir: 'dist',
   },
