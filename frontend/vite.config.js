d985e2496a884902909ec5b1b8194ea57553f1ac
Shobhit Katiyar <Shobhitkatiyar639@gmail.com>
2025-09-21 12:49:34 +0530

    deploying frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,33 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-  tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  base: './', // super important for static hosting like Vercel
   optimizeDeps: {
     include: ["axios"],
   },
   build: {
-    rollupOptions: {
-      external: ['axios', 'react-toastify']
-    }
+    outDir: 'dist',
   },
   server: { port: 5173 }
 })
-
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// export default defineConfig({
-//   plugins: [react()],
-//   optimizeDeps: {
-//     include: ["axios"],
-//   },
-//   build: {
-//     rollupOptions: {
-//       external: [],
-//     },
-//   },
-// });
-
